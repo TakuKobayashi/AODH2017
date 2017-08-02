f2e1ea5905c48c80e6b51b85c993d9b00e623e32
TakuKobayshi <>
2017-08-02 17:32:54 +0900

    Android Project With Kotolin first commit

---
+++ Android/app/src/main/java/com/kobayshi/taku/aodh2017/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kobayshi.taku.aodh2017
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
